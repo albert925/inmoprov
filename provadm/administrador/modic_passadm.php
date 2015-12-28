ef8622f79820eb16009768c9d0d144d35f8c6fea
albert <albertarias925@gmail.com>
2015-12-28 12:01:11 -0500

    cifra

--- provadm/administrador/modic_passadm.php
+++ provadm/administrador/modic_passadm.php
@@ -3,15 +3,21 @@
 	$idR=$_POST['fb'];
 	$conact=$_POST['b'];
 	$connew=$_POST['c'];
+	function passcifr($pass)
+	{
+		$salt = 'sanae-pequena-nina-7-years$/';
+		$c = sha1(md5($salt.$pass));
+		return $c;
+	}
 	if ($idR=="" || $conact=="" || $connew=="") {
 		echo "1";
 	}
 	else{
-		$existe="SELECT * from administrador where id_adm=$idR and pass_adm='$conact'";
+		$existe="SELECT * from administrador where id_adm=$idR and pass_adm='".passcifr($conact)."'";
 		$sql_existe=mysql_query($existe,$conexion) or die (mysql_error());
 		$numero=mysql_num_rows($sql_existe);
 		if ($numero>0) {
-			$modificar="UPDATE administrador set pass_adm='$connew' where id_adm=$idR";
+			$modificar="UPDATE administrador set pass_adm='".passcifr($connew)."' where id_adm=$idR";
 			mysql_query($modificar,$conexion) or die (mysql_error());
 			echo "3";
 		}
--- provadm/ingr_adm.php
+++ provadm/ingr_adm.php
@@ -2,11 +2,13 @@
 	include '../config.php';
 	$a=$_POST['a'];//usuario
 	$b=$_POST['b'];//contraseña
+	$salt = 'sanae-pequena-nina-7-years$/';
 	if ($a=="" || $b=="") {
 		echo "1";
 	}
 	else{
-		$existe="SELECT * from administrador where user_adm='$a' and pass_adm='$b'";
+		$c = sha1(md5($salt.$b));
+		$existe="SELECT * from administrador where user_adm='$a' and pass_adm='$c'";
 		$sql_existe=mysql_query($existe,$conexion) or die (mysql_error());
 		$numero=mysql_num_rows($sql_existe);
 		if ($numero>0) {
